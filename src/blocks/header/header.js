dd2fbd5c64540bfa5c8e41101964f206cb161bde
motosasha <moto.sasha@gmail.com>
2024-12-08 17:09:58 +0300

    feat: add search field to mobile

--- src/blocks/header/header.js
+++ src/blocks/header/header.js
@@ -5,10 +5,17 @@ ready(function () {
   const headerHost = document.querySelector(".header");
   if (headerHost) {
     const burgerTriggers = document.querySelectorAll("[data-burger]");
+    const searchTrigger = document.querySelector("[data-search]");
+    const searchForm = document.querySelector("#mainSearch");
+
     burgerTriggers.forEach((trigger) => {
       trigger.addEventListener("click", () => {
         openNav(document.body, "open-nav");
       });
+    });
+
+    searchTrigger.addEventListener("click", () => {
+      searchForm.classList.toggle("header__search--active");
     });
   }
 });
